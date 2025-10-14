bf55403deb24e6978e66ccede43ff62afb0cea46
John Tao <zytao24@gmail.com>
2025-10-14 22:16:09 +0800

    chore: config custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://johntao.github.io',
-  base: 'blog-2025',
+  // site: 'https://johntao.github.io',
+  // base: 'blog-2025',
+  site: 'https://www.johntao.dev',
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
