6646d568e17219801f095fafaef01aa4530be562
John Tao <zytao24@gmail.com>
2025-10-14 11:58:38 +0800

    feat: introduce content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,25 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    tags: z.array(z.string()).default([]),
-  }),
+const postSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  tags: z.array(z.string()).default([]),
 });
 
-export const collections = { blog };
+const author1 = defineCollection({
+  type: 'content',
+  schema: postSchema,
+});
+
+const author2 = defineCollection({
+  type: 'content',
+  schema: postSchema,
+});
+
+const author3 = defineCollection({
+  type: 'content',
+  schema: postSchema,
+});
+
+export const collections = { author1, author2, author3 };
