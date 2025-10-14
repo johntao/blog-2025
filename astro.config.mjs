28a4d72bda9827f293e94765a55c14715f2d10f9
John Tao <zytao24@gmail.com>
2025-10-14 15:27:34 +0800

    devops: config GitHub deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://johntao.github.io',
+  base: 'blog-2025',
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
